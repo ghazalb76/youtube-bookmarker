775ea3a49069d821af83407da77c541b58576e3f
Ghazaleh Bakhtiariazad <ghazal.b93@gmail.com>
2026-06-09 15:07:10 -0700

    Add the structures

---
+++ contentScript.js
@@ -0,0 +1,9 @@
+const fetchBookmarks = () => {};
+
+const addNewBookmarkEventHandler = async () => {};
+
+const newVideoLoaded = async () => {};
+
+chrome.runtime.onMessage.addListener((obj, sender, response) => {});
+
+const getTime = t => {};
